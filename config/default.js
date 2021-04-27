77a4ef00f29d85e3602d18b08dfcd41a0f7718dd
钱丽云 <376084782@qq.com>
2021-04-27 22:07:27 +0800

    no message

--- config/default.js
+++ config/default.js
@@ -3,5 +3,5 @@ module.exports = {
 	port: parseInt(process.env.PORT, 10) || 13009,
 	wssPort: 13110,
 	// url: 'mongodb://127.0.0.1:27017/pet'
-	url: 'mongodb://39.106.211.15:27017/malasong'
+	url: 'mongodb://127.0.0.1:27017/rubish'
 }
